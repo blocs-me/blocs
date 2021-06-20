cc827a6e775ade4b854d5d88c3db8fe9884030fd
Moniet <moniet.sawhney@gmail.com>
2021-06-21 01:11:00 +0530

    📝 added auth to landing page (shows avatar on success)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import LandingPage from "../components/LandingPage"
 import { A, NavLink } from "../components/Nav"
+import useAuth from "../hooks/useAuth"
 
 function Landing() {
+  useAuth()
+
   return <LandingPage />
 }
 
