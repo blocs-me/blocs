991404cce5d93b14df68d615d7954700ffe06c42
Moniet <moniet.sawhney@gmail.com>
2021-06-20 12:07:07 +0530

    📝 responsive styles for beta wrapper

--- src/components/BetaWrapper/index.js
+++ src/components/BetaWrapper/index.js
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled"
 import { color } from "styled-system"
 import tg from "@styled-system/theme-get"
+import theme from "../../styles/theme"
 
 const BetaWrapper = styled.div`
   display: inline-block;
@@ -11,17 +12,28 @@ const BetaWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    font-size: 0.5rem;
+    font-family: inherit;
     content: "beta";
-    padding: 1px 2px;
-    border-radius: 6px;
+    padding: 3px 4px;
     background: ${tg("colors.secondary")};
     color: ${tg("colors.primary.lightest")};
     font-weight: 400;
     position: absolute;
+    font-size: 0.5rem;
+    line-height: 1;
+    border-radius: 10px;
+    letter-spacing: ${tg("letterSpacings.sm")};
     top: 0;
     right: 0;
     transform: translateX(calc(100%));
+  }
+
+  @media (max-width: ${theme.breakpoints[3]}) {
+    &::after {
+      font-size: 0.4rem;
+      padding: 3px;
+      border-radius: 5px;
+    }
   }
 `
 
