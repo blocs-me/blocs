07cbc79edcfffae4832a9962a12d35c0bc4b5b96
Moniet <moniet.sawhney@gmail.com>
2021-06-21 01:23:48 +0530

    📝📈 added cloudflar analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,21 @@
+import Head from "next/head"
 import { ThemeProvider } from "@emotion/react"
-import { useMediaQuery } from "beautiful-react-hooks"
 import Nav from "../components/Nav"
 import GlobalProvider from "../contexts/GlobalContextProvider/GlobalContextProvider"
 import GlobalStyle from "../styles/GlobalStyle"
 import Reset from "../styles/Reset"
-import theme, { darkModeColors } from "../styles/theme"
+import theme from "../styles/theme"
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <Script
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "a9429c8dbd674c38bf769344e4abc7d8"}'
+        />
+      </Head>
       <GlobalProvider>
         <Reset />
         <GlobalStyle />
