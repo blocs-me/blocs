a0efb8a4134c7bdfe84482fe21e2c8e3e66d0193
Moniet <moniet.sawhney@gmail.com>
2021-06-24 15:26:20 +0530

    📦 added next-sitemap and config

---
+++ next-sitemap.js
@@ -0,0 +1,14 @@
+const sitemap = {
+  siteUrl: "https://blocs.me",
+  generateRobotsTxt: false,
+  exclude: [
+    "/dashboard/*",
+    "/main-analytics",
+    "/main-habit-tracker",
+    "/main-pomodoro",
+    "/main-streaks",
+    "/waiter-tracker",
+  ],
+}
+
+module.exports = sitemap
