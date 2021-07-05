570d36e3f0e5b669602f338170c4640c02db54ec
Moniet <moniet.sawhney@gmail.com>
2021-07-05 13:27:47 +0530

    🧐 remove discord webhook url

--- src/utils/paths.js
+++ src/utils/paths.js
@@ -10,5 +10,3 @@ const getPath = (path) => `${BASE_URL}${path}`
 
 export const USER_PATH = getPath("/users")
 export const VALIDATE_USER_AUTH_PATH = getPath("/auth/validate")
-export const DISCORD_USER_SIGNUPS_WEBOOK =
-  "https://discord.com/api/webhooks/861179086280982539/feZRPcZ4u781vaZ1Bh3ZV64hrIhuLor3_wODIJcUrZ22nYst45pcickxslhlmsxC1_52"
