92503d8697c0dd89601198fce04410fdfe3acecb
Moniet <moniet.sawhney@gmail.com>
2021-08-16 18:02:56 +0530

    📝 created notifs component

---
+++ src/components/design-system/Notifications/index.js
@@ -0,0 +1,111 @@
+import { useTheme } from "@emotion/react"
+import slideIn from "@/keyframes/slideIn"
+import Flex from "@/helpers/Flex"
+import Text from "@/design-system/Text"
+
+import useNotifications, {
+  ERROR_NOTIF,
+  INFO_NOTIF,
+  SUCCESS_NOTIF,
+} from "./useNotifications"
+
+const getNotifBgColor = (theme, notifType) => {
+  switch (notifType) {
+    case ERROR_NOTIF:
+      return theme.colors.danger
+    case INFO_NOTIF:
+    case SUCCESS_NOTIF:
+      return theme.colors.success
+    default:
+      return theme.colors.success
+  }
+}
+
+const NotifItem = ({ type, content = "", isLastItem }) => {
+  const theme = useTheme()
+  const bgColor = getNotifBgColor(theme, type)
+
+  return (
+    <div
+      css={{
+        position: "absolute",
+        top: 0,
+        animation: `${slideIn} 0.2s ease forwards`,
+      }}
+    >
+      <Flex
+        borderRadius="lg"
+        boxShadow="lg"
+        bg={bgColor}
+        width="fit-content"
+        zIndex="notification"
+        minWidth="200px"
+        minHeight="50px"
+        alignItems="center"
+        justifyContent="center"
+        p="xs"
+        color="primary.accent-1"
+        style={{
+          transform: !isLastItem ? "scale(0.9) translateY(10px)" : "none",
+          opacity: isLastItem ? 1 : 0.7,
+        }}
+        css={{
+          textAlign: "center",
+          transition: "0.2s opacity ease, transform 0.2s ease",
+        }}
+      >
+        <Text
+          fontSize="xs"
+          textAlign="center"
+          fontWeight="300"
+          lineHeight={1.5}
+          m={0}
+        >
+          {content}
+        </Text>
+      </Flex>
+    </div>
+  )
+}
+
+const NotifContainer = (props) => {
+  const { useNotifs } = useNotifications()
+  const notifs = useNotifs()
+
+  return (
+    <Flex
+      position="absolute"
+      pd="md"
+      top="0"
+      left="50%"
+      width="100%"
+      justifyContent="center"
+      p="sm"
+      css={{ transform: "translate(-50%)" }}
+      {...props}
+    >
+      <Flex flexDirection="column" position="relative" alignItems="center">
+        {notifs.slice(0, 2).map((item) => (
+          <NotifItem
+            {...item}
+            key={item.id}
+            isLastItem={notifs.slice(-1)[0].id === item.id}
+          />
+        ))}
+      </Flex>
+    </Flex>
+  )
+}
+
+const Notifications = (props) => {
+  const { NotifProvider } = useNotifications()
+
+  return (
+    <NotifProvider>
+      {props.children}
+      <NotifContainer {...props} />
+    </NotifProvider>
+  )
+}
+
+export default Notifications
---
+++ src/components/design-system/Notifications/useNotifications.js
@@ -0,0 +1,79 @@
+import makeStore from "src/lib/makeStore"
+
+export const ERROR_NOTIF = "ERROR_NOTIF"
+export const SUCCESS_NOTIF = "SUCCESS_NOTIF"
+export const INFO_NOTIF = "INFO_NOTIF"
+export const DELETE_NOTIF = "DELETE_NOTIF"
+
+const initialState = []
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ERROR_NOTIF:
+      return [
+        ...state,
+        { content: action.content, type: ERROR_NOTIF, id: action.id },
+      ]
+    case SUCCESS_NOTIF:
+      return [
+        ...state,
+        { content: action.content, type: SUCCESS_NOTIF, id: action.id },
+      ]
+    case INFO_NOTIF:
+      return [
+        ...state,
+        { content: action.content, type: INFO_NOTIF, id: action.id },
+      ]
+    case DELETE_NOTIF:
+      const notifIndex = state.findIndex((notif) => notif.id === action.notifId)
+      return [...state.slice(0, notifIndex), ...state.slice(notifIndex + 1)]
+    default:
+      return state
+  }
+}
+
+const [Provider, useStore, useDispatch] = makeStore({
+  initialState,
+  reducer,
+})
+
+const useNotifications = () => {
+  const notifs = useStore()
+  const dispatch = useDispatch()
+
+  const DELETE_NOTIF_AFTER = 3000 // ms
+
+  const deleteNotifAction = (notifId) =>
+    dispatch({
+      notifId,
+      type: DELETE_NOTIF,
+    })
+
+  const createNotifAction = (content, type) => {
+    const id = Math.random()
+    dispatch({
+      id,
+      content,
+      type,
+    })
+
+    setTimeout(() => {
+      deleteNotifAction(id)
+    }, DELETE_NOTIF_AFTER)
+  }
+
+  const createError = (content) => createNotifAction(content, ERROR_NOTIF)
+  const createSuccess = (content) => createNotifAction(content, SUCCESS_NOTIF)
+  const createInfo = (content) => createNotifAction(content, INFO_NOTIF)
+
+  return {
+    NotifProvider: Provider,
+    notifs,
+    createError,
+    createInfo,
+    createSuccess,
+    useNotifs: useStore,
+  }
+}
+
+export default useNotifications
