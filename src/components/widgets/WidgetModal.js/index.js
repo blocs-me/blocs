cc35d6413845a16f4706d8254d16e4d2431f6bc3
Moniet <moniet.sawhney@gmail.com>
2021-08-23 12:49:38 +0530

    📝 added exit icon to widget modal

--- src/components/widgets/WidgetModal.js/index.js
+++ src/components/widgets/WidgetModal.js/index.js
@@ -1,7 +1,8 @@
 import Flex from "@/helpers/Flex"
+import Icon from "@/helpers/Icon"
 import { useClickOutside } from "@/hooks/useClickOutside"
 import { useRef } from "react"
-import CrossIcon from '../../../icons/cross.svg'
+import CrossIcon from "../../../icons/cross.svg"
 
 const { useTheme } = require("@emotion/react")
 const {
@@ -52,10 +53,30 @@ const WidgetModal = ({
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 40 }}
           >
-            <Card width="100%" height="100%" p={p} position="relative"ref={containerRef}>
+            <Card
+              width="100%"
+              height="100%"
+              p={p}
+              position="relative"
+              ref={containerRef}
+            >
               {children}
-              <Flex>
-
+              <Flex
+                onClick={(e) => hideModal(e)}
+                as="button"
+                position="absolute"
+                top={0}
+                left={0}
+                css={{ transform: "translate(-25%, -25%)" }}
+                bg="danger"
+                size="25px"
+                borderRadius="50%"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Icon fill="primary.accent-1" p="1px">
+                  <CrossIcon />
+                </Icon>
               </Flex>
             </Card>
           </m.div>
