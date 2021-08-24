4113384607bf7891ab65913278edb0278b803974
Moniet <moniet.sawhney@gmail.com>
2021-08-24 18:21:43 +0530

    🐞 removed incorrect imports

--- src/pages/pomodoro/[[...slug]].js
+++ src/pages/pomodoro/[[...slug]].js
@@ -1,13 +1,7 @@
-import ClientSideOnly from "@/helpers/ClientSideOnly"
-import Flex from "@/helpers/Flex"
-import useColorMode, { ColorModeProvider } from "@/hooks/useColorMode"
+import { ColorModeProvider } from "@/hooks/useColorMode"
 import { WidgetAuthProvider } from "@/hooks/useWidgetAuth"
 import Pomodoro from "@/widgets/Pomodoro"
-import { PomodoroProvider } from "@/widgets/Pomodoro/usePomodoroStore"
 import WidgetPage from "@/widgets/WidgetPage"
-import { ThemeProvider } from "@emotion/react"
-import { useMediaQuery } from "beautiful-react-hooks"
-import theme, { darkModeColors } from "src/styles/theme"
 
 export default function MainPomodoro() {
   return (
