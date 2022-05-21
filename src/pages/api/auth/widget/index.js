3d4e9241ec015fa440b64ccb003af5b0a54ef92e
Moniet <moniet.sawhney@gmail.com>
2022-05-21 16:37:17 +0200

    ✨ prettification

--- src/pages/api/auth/widget/index.js
+++ src/pages/api/auth/widget/index.js
@@ -1,4 +1,4 @@
-import cors from "@/lambda/auth/cors"
+import cors from "@/lambda/middlewares/apiGuard/cors"
 import Rest from "@/lambda/lib/rest"
 import loginWidgetUser from "@/lambda/routers/auth/loginWidgetUser"
 
--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,19 +1,19 @@
-import notionOAuthData from "./notionOAuthData"
+import notionOAuthData from './notionOAuthData'
 
 const BASE_URL =
-  process.env.NEXT_PUBLIC_ENV?.toLowerCase() === "local"
-    ? "http://localhost:3000/api"
-    : "/api"
+  process.env.NEXT_PUBLIC_ENV?.toLowerCase() === 'local'
+    ? 'http://localhost:3000/api'
+    : '/api'
 
 const getPath = (path) => `${BASE_URL}${path}`
 
 const { CLIENT_ID, REDIRECT_URL } = notionOAuthData
 
-export const USER_PATH = getPath("/users")
-export const VALIDATE_USER_AUTH_PATH = getPath("/auth/validate")
-export const SETTINGS_PATH = getPath("/pomodoro/settings")
-export const POMODORO_PRESETS_PATH = getPath("/pomodoro/presets")
-export const TEMP_ACCESS_TOKEN_PATH = getPath("/auth/temp-access-token")
-export const WIDGET_LOGIN_PATH = getPath("/auth/widget")
-export const WIDGET_LOGIN_VALIDATION_PATH = getPath("/auth/widget/validate")
+export const USER_PATH = getPath('/users')
+export const VALIDATE_USER_AUTH_PATH = getPath('/auth/validate')
+export const SETTINGS_PATH = getPath('/pomodoro/settings')
+export const POMODORO_PRESETS_PATH = getPath('/pomodoro/presets')
+export const TEMP_ACCESS_TOKEN_PATH = getPath('/widget/access-token')
+export const WIDGET_LOGIN_PATH = getPath('/auth/widget')
+export const WIDGET_LOGIN_VALIDATION_PATH = getPath('/auth/widget/validate')
 export const NOTION_OAUTH_URL_REDIRECT_URL = `https://api.notion.com/v1/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code&owner=user`
