b62b4d0a0d6a3ef0328efc49eb21f6ffc200729e
Moniet Sawhney <moniet.sawhney@gmail.com>
2022-06-30 20:49:50 +0000

    added gap to flex component

--- src/components/helpers/Flex/index.js
+++ src/components/helpers/Flex/index.js
@@ -1,9 +1,17 @@
 import styled from "@emotion/styled"
-import { flexbox } from "styled-system"
+import { flexbox, system } from "styled-system"
 import Box from "../Box"
+
+const gap = system({ 
+  gap: {
+    property: 'gap',
+    scale: 'space'
+  }
+})
 
 const Flex = styled(Box)`
   display: flex;
+  ${gap}
   ${flexbox}
   ${({ center }) => {
     if (center) {
