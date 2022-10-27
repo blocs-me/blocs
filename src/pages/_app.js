fc7cd5d912071c7edd0c5b8c9cf14666824aee27
Moniet <moniet.sawhney@gmail.com>
2022-10-27 14:04:27 +0200

    📝 replaced cloudflare with splitbee analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,36 +1,32 @@
-import Head from "next/head"
-import { ThemeProvider } from "@emotion/react"
-import GlobalProvider from "../contexts/GlobalContextProvider/GlobalContextProvider"
-import GlobalStyle from "../styles/GlobalStyle"
-import Reset from "../styles/Reset"
-import theme from "../styles/theme"
-import { useRouter } from "next/router"
-import Nav from "@/design-system/Nav"
+import Head from 'next/head'
+import { ThemeProvider } from '@emotion/react'
+import GlobalProvider from '../contexts/GlobalContextProvider/GlobalContextProvider'
+import GlobalStyle from '../styles/GlobalStyle'
+import Reset from '../styles/Reset'
+import theme from '../styles/theme'
+import { useRouter } from 'next/router'
+import Nav from '@/design-system/Nav'
 
 function MyApp({ Component, pageProps }) {
   const { pathname } = useRouter()
 
   const getNavTitle = () => {
-    if (pathname?.includes("dashboard")) return "DASHBOARD"
-    if (pathname?.includes("pricing")) return "PRICING"
-    return ""
+    if (pathname?.includes('dashboard')) return 'DASHBOARD'
+    if (pathname?.includes('pricing')) return 'PRICING'
+    return ''
   }
 
   const showNav = (() => {
-    if (pathname?.includes("dashboard")) return true
-    if (pathname?.includes("pricing")) return true
-    if (pathname === "/") return true
+    if (pathname?.includes('dashboard')) return true
+    if (pathname?.includes('pricing')) return true
+    if (pathname === '/') return true
     return false
   })()
 
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <script
-          defer
-          src="https://static.cloudflareinsights.com/beacon.min.js"
-          data-cf-beacon='{"token": "a9429c8dbd674c38bf769344e4abc7d8"}'
-        />
+        <script async src="https://cdn.splitbee.io/sb.js"></script>
       </Head>
       <GlobalProvider>
         <Reset />
