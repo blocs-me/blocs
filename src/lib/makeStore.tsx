2b4bb0afeaa356e1d4c61e8e1a28fccc6e925c04
Moniet <moniet.sawhney@gmail.com>
2022-11-25 22:39:24 +0100

    typing making store hook

--- src/lib/makeStore.tsx
+++ src/lib/makeStore.tsx
@@ -1,12 +1,18 @@
-import React, { useReducer, useContext } from "react"
+import React, { useReducer, useContext, ReactNode, Provider } from 'react'
+import { WithChildren } from '@/utils/tsUtils'
+
+export type Action<T = string, PL = any> = {
+  type: T
+  payload: PL
+}
 
 export default function makeStore({ initialState, reducer }) {
-  const context = React.createContext()
-  const dispatchContext = React.createContext()
+  const context = React.createContext({})
+  const dispatchContext = React.createContext({})
   const useDispatch = () => useContext(dispatchContext)
   const useStore = () => useContext(context)
 
-  const Provider = ({ children }) => {
+  const Provider = ({ children }: WithChildren<{}>) => {
     const [state, dispatch] = useReducer(reducer, initialState)
 
     return (
