c93c350f72d86f1b7bca67f13291ab238c123ca8
Moniet <moniet.sawhney@gmail.com>
2022-12-01 17:52:55 +0100

    change json validator folder

--- src/lambda-functions/lib/restValidator/jsonValidator.js
+++ src/lambda-functions/lib/restValidator/jsonValidator.js
@@ -1,8 +1,8 @@
 import Ajv from 'ajv'
 
-import pomodoroPresetSchema from '../schemas/pomdoro/pomodoroPresetSchema'
-import waterTrackerSettingsSchema from '../schemas/water-tracker/waterTrackerSettingsSchema'
-import createWidgetTokenSchema from '../schemas/shared-widget-schemas/createWidgetTokenSchema'
+import pomodoroPresetSchema from './schemas/pomdoro/pomodoroPresetSchema'
+import waterTrackerSettingsSchema from './schemas/water-tracker/waterTrackerSettingsSchema'
+import createWidgetTokenSchema from './schemas/shared-widget-schemas/createWidgetTokenSchema'
 
 const ajv = new Ajv()
 
