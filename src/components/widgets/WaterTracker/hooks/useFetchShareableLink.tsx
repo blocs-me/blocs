7658bf10672154783cefea40f88a879fe21d6927
Moniet <moniet.sawhney@gmail.com>
2022-12-05 23:37:32 +0100

    Revert "📝 replace useUrlHash with useRouter"
    This reverts commit 637c480f5987703ca521d40eadf9a0b296ff2619.

--- src/components/widgets/WaterTracker/hooks/useFetchShareableLink.tsx
+++ src/components/widgets/WaterTracker/hooks/useFetchShareableLink.tsx
@@ -2,13 +2,12 @@ import useFetch from '@/hooks/useFetch'
 import useUrlHash from '@/hooks/useUrlHash/useUrlHash'
 import { SHAREABLE_TOKEN_PATH } from 'src/utils/endpoints'
 import { UrlHash } from '../types'
-import { useRouter } from 'next/router'
 
 const getEndpoint = (widgetType, widgetToken) =>
   `${SHAREABLE_TOKEN_PATH}?widgetType=${widgetType}&widgetToken=${widgetToken}`
 
 const useFetchShareableLink = () => {
-  const hash = useRouter().query as UrlHash
+  const hash = useUrlHash<UrlHash>()
 
   const { data, fetcher, loading, error } = useFetch(
     getEndpoint('WATER_TRACKER', hash['token']),
