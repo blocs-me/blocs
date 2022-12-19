6c98c81c33edc96c6596736775cf54843e6e60b7
Moniet <moniet.sawhney@gmail.com>
2022-12-19 11:33:43 +0100

    remove pomodoro banner

--- src/components/widgets/Pomodoro/PomodoroMainPage/index.js
+++ src/components/widgets/Pomodoro/PomodoroMainPage/index.js
@@ -84,16 +84,16 @@ const PomodoroMainPage = () => {
     }
   }, [presets, currentPreset]) // eslint-disable-line
 
-  useEffect(() => {
-    const hideBanner = storage.getItem('blocs-release-banner') === 'true'
-    if (isLoggedIn && !hideBanner) {
-      storage.setItem('blocs-release-banner', 'true')
-      notifs.createInfo(
-        'New version of blocs is releasing 16th December 🎉🥳',
-        5000
-      )
-    }
-  }, [isLoggedIn, notifs])
+  // useEffect(() => {
+  //   const hideBanner = storage.getItem('blocs-release-banner') === 'true'
+  //   if (isLoggedIn && !hideBanner) {
+  //     storage.setItem('blocs-release-banner', 'true')
+  //     notifs.createInfo(
+  //       'New version of blocs is releasing 16th December 🎉🥳',
+  //       5000
+  //     )
+  //   }
+  // }, [isLoggedIn, notifs])
 
   const handleMouseOver = (e) => {
     if (widgetLayout?.contains(e.target)) {
