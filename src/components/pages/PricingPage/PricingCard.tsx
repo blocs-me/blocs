53e1ec4b68dfb510130186baa837d7266f4ab35a
Moniet <moniet.sawhney@gmail.com>
2023-01-15 18:41:35 +0100

    [wip] pricing page

---
+++ src/components/pages/PricingPage/PricingCard.tsx
@@ -0,0 +1,106 @@
+import Button from '@/design-system/Button'
+import Text from '@/design-system/Text'
+import Box from '@/helpers/Box'
+import Flex from '@/helpers/Flex'
+import Stack from '@/helpers/Stack'
+import styled from '@emotion/styled'
+import { MouseEvent, ReactNode } from 'react'
+import { IBox } from '../../helpers/Box/Box.types'
+import themeGet from '@styled-system/theme-get'
+
+type Props = {
+  header: string
+  price: string
+  priceDescSmall: string
+  priceDescLarge: string
+  onClick: (e: MouseEvent) => void
+  ctaColor?: string
+  cta: string
+  isPremium: boolean
+  children?: ReactNode
+}
+
+const ChildrenContainer = styled.div`
+  & > p:not(:first-of-type) {
+    margin-top: 1rem;
+  }
+
+  & > p {
+    margin-bottom: 0.8rem;
+  }
+
+  & [data-type='checkbox'] {
+    margin-top: 0.25rem;
+  }
+`
+
+const PricingCard = ({
+  header,
+  price,
+  priceDescLarge,
+  priceDescSmall,
+  onClick,
+  cta,
+  ctaColor = 'foreground',
+  children,
+  isPremium,
+  ...rest
+}: Props & IBox) => {
+  return (
+    <Flex
+      position="relative"
+      borderRadius="md"
+      bg="background"
+      boxShadow="default"
+      p="sm"
+      flexDirection="column"
+      width="300px"
+      css={{ gap: '1rem' }}
+      {...rest}
+    >
+      <Text as="h3" variant="mediumBold" m={0}>
+        {header}
+      </Text>
+
+      <Flex alignItems="center">
+        <Text
+          fontSize="xl"
+          fontWeight="bold"
+          color="foreground"
+          m={0}
+          mr="sm"
+          lineHeight={1}
+        >
+          ${price}
+        </Text>
+        <Text variant="pSmall" mt={0} lineHeight={1}>
+          {priceDescSmall}
+        </Text>
+      </Flex>
+      <Text variant="pSmall">{priceDescLarge}</Text>
+      <Button
+        width="100%"
+        py="sm"
+        bg={ctaColor}
+        color="background"
+        onClick={onClick}
+        fontSize="sm"
+        borderRadius="sm"
+      >
+        {cta}
+      </Button>
+      <Box width="100%" height="1px" bg="primary.accent-1" />
+      <div>
+        <Text color="foreground" fontSize="sm" fontWeight="bold" mb={0}>
+          Features:
+        </Text>
+        <ChildrenContainer>
+          <Text variant="pSmall">Widgets:</Text>
+          {children}
+        </ChildrenContainer>
+      </div>
+    </Flex>
+  )
+}
+
+export default PricingCard
---
+++ src/components/pages/PricingPage/index.tsx
@@ -0,0 +1,165 @@
+import Footer from '@/design-system/Footer'
+import Nav from '@/design-system/Nav'
+import Text from '@/design-system/Text'
+import Box from '@/helpers/Box'
+import Flex from '@/helpers/Flex'
+import { keyframes } from '@emotion/react'
+import PricingCard from './PricingCard'
+import PricingCardCheckbox from './PricingCardCheckbox'
+
+const float = keyframes`
+    from {
+      transform: translateY(-2px);
+    } to {
+      transform: translateY(1px);
+    }
+`
+
+const PricingPage = () => {
+  return (
+    <>
+      <Flex pt="80px" flexDirection="column" bg="background" maxWidth="100vw">
+        <Nav />
+        <Flex
+          width="100%"
+          height="100%"
+          alignItems="center"
+          pt="md"
+          pb="xl"
+          flexDirection="column"
+          px="md"
+        >
+          <div>
+            <Text
+              as="h1"
+              color="foreground"
+              mb="sm"
+              fontWeight={'bold'}
+              fontSize="xl"
+              textAlign="center"
+            >
+              Flexible Pricing Plans
+            </Text>
+            <Text
+              as="h2"
+              mt={0}
+              fontSize="md"
+              fontWeight={200}
+              letterSpacing={'md'}
+              color="primary.accent-4"
+              textAlign="center"
+            >
+              Choose a plan that works best for you!
+            </Text>
+          </div>
+
+          <Flex
+            css={{ gap: '1.8rem' }}
+            width="100%"
+            justifyContent="center"
+            flexWrap="wrap"
+            mt="md"
+          >
+            <PricingCard
+              header="Free Trial"
+              price="0"
+              priceDescSmall="Free 14 day trial to try out premium features"
+              priceDescLarge="Basic features will always be free!"
+              cta="Try for free"
+              isPremium={false}
+              onClick={(e) => {}}
+            >
+              <PricingCardCheckbox text="Pomodoro" />
+              <PricingCardCheckbox text="Habit Tracker" isChecked={false} />
+              <PricingCardCheckbox text="Water Tracker" isChecked={false} />
+
+              <Text variant="pSmall">Analytics:</Text>
+              <PricingCardCheckbox text="One week of data retention" />
+              <PricingCardCheckbox
+                text="Unlimited analytics data retention"
+                isChecked={false}
+              />
+              <PricingCardCheckbox
+                text="Weekly / monthly progress views"
+                isChecked={false}
+              />
+            </PricingCard>
+            <PricingCard
+              header="Lifetime Access"
+              price="30"
+              priceDescSmall="unlimited access to all future widgets"
+              priceDescLarge="Pay once and then never again!"
+              cta="Buy now"
+              ctaColor="brand.accent-1"
+              isPremium
+              onClick={(e) => {}}
+              css={{ transform: 'scale(1.05)' }}
+              boxShadow="lg"
+            >
+              <PricingCardCheckbox text="Pomodoro" />
+              <PricingCardCheckbox text="Water Analytics" />
+              <PricingCardCheckbox text="Habit Tracker" />
+              <PricingCardCheckbox text="All future widgets..." />
+              <Text variant="pSmall">Analytics:</Text>
+              <PricingCardCheckbox text="Unlimited analytics data retention" />
+              <PricingCardCheckbox text="Weekly / monthly progress views" />
+
+              <Text variant="pSmall">Extras:</Text>
+              <PricingCardCheckbox text="Share your progress with friends" />
+              <PricingCardCheckbox text="Save data to notion (coming soon)" />
+              <Box
+                position="absolute"
+                color="background"
+                bg="brand.accent-1"
+                borderRadius="10px"
+                top="sm"
+                right="sm"
+                css={{
+                  animation: `${float} 1s ease-in-out infinite alternate`
+                }}
+              >
+                <Text
+                  fontSize="xxs"
+                  m={0}
+                  py="3px"
+                  px="xs"
+                  lineHeight={1.75}
+                  textAlign="center"
+                >
+                  <span>Limited</span>
+                  <br />
+                  {/* TODO: Show realtime data for countdown 👇 */}
+                  {/* <small>1000 left</small> */}
+                </Text>
+              </Box>
+            </PricingCard>
+            <PricingCard
+              header="Per widget"
+              price="5"
+              priceDescSmall="Access premium features of the purchased widget"
+              priceDescLarge="Own your widget forever!"
+              cta="Buy a widget"
+              isPremium
+              onClick={(e) => {}}
+            >
+              <PricingCardCheckbox text="Pomodoro" />
+              <PricingCardCheckbox text="or Water Analytics" />
+              <PricingCardCheckbox text="or Habit Tracker" />
+              <Box height="20px" />
+              <Text variant="pSmall">Analytics:</Text>
+              <PricingCardCheckbox text="Unlimited analytics data retention" />
+              <PricingCardCheckbox text="Weekly / monthly progress views" />
+
+              <Text variant="pSmall">Extras:</Text>
+              <PricingCardCheckbox text="Share your progress with friends" />
+              <PricingCardCheckbox text="Save data to notion (coming soon)" />
+            </PricingCard>
+          </Flex>
+        </Flex>
+        <Footer />
+      </Flex>
+    </>
+  )
+}
+
+export default PricingPage
