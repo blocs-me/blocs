b68dc2e1702e9ac794b9cbd79960f5eaa550252c
Moniet <moniet.sawhney@gmail.com>
2023-01-23 22:25:31 +0100

    ✨ add gap prop to flex helper

--- src/components/helpers/Flex/index.ts
+++ src/components/helpers/Flex/index.ts
@@ -1,20 +1,30 @@
 import styled from '@emotion/styled'
 import { flexbox, FlexboxProps } from 'styled-system'
 import Box from '../Box'
+import shouldForwardProp from '@styled-system/should-forward-prop'
+import { system } from 'styled-system'
 
-const Flex = styled(Box)<{ center?: boolean } & FlexboxProps>`
-  display: flex;
-  ${flexbox}
-  ${({ center }) => {
+const gap = system({
+  gap: {
+    property: 'gap',
+    scale: 'space'
+  }
+})
+
+const Flex = styled(Box, {
+  shouldForwardProp
+})<{ center?: boolean } & FlexboxProps>(
+  flexbox,
+  gap,
+  { display: 'flex' },
+  ({ center }) => {
     if (center) {
-      return `
-      justify-content: center;
-      alignItems: center;
-      `
+      return {
+        justifyContent: 'center',
+        alignItems: 'center'
+      }
     }
-
-    return ''
-  }}
-`
+  }
+)
 
 export default Flex
