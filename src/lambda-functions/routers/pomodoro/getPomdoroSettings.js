707b0f1caf6dd6bdfb2656e1ddd698de1ba6f9a5
Moniet <moniet.sawhney@gmail.com>
2023-01-27 23:38:49 +0100

    ✨ added to do (tech debt)

--- src/lambda-functions/routers/pomodoro/getPomdoroSettings.js
+++ src/lambda-functions/routers/pomodoro/getPomdoroSettings.js
@@ -1,16 +1,17 @@
-import faunaClient from "@/lambda/faunaClient"
-import { query as q } from "faunadb"
+import faunaClient from '@/lambda/faunaClient'
+import { query as q } from 'faunadb'
 
+//TODO: validate and delete this file
 const getPomdoroSettings = async (req, res) => {
   // const { id }  = req.user
 
   try {
     const faunaRes = await faunaClient.query(
-      q.Call(q.Function("pomodoro_settings_by_user_id"), 303985067693179406) // add user id dynamically
+      q.Call(q.Function('pomodoro_settings_by_user_id'), 303985067693179406) // add user id dynamically
     )
 
     res.status(200).json({
-      data: faunaRes.data,
+      data: faunaRes.data
     })
   } catch (error) {
     console.error(error)
