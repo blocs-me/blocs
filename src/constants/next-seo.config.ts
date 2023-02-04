f8bb05d2de3bca486ba03149ef773586cd4780f0
Moniet <moniet.sawhney@gmail.com>
2023-02-04 14:26:32 +0100

    ✨ next seo

---
+++ src/constants/next-seo.config.ts
@@ -0,0 +1,20 @@
+const nextSeoConfig = {
+  openGraph: {
+    title: 'blocs | notion widgets for habit tracking',
+    type: 'website',
+    description:
+      'Pricing for blocs widgets | €30 for lifetime access | €4 for any blocs notion widget; own it forever',
+    images: [
+      {
+        url: 'https://www.blocs.me/blocs-social-banner.png',
+        alt: 'Blocs notion widget social banner'
+      }
+    ]
+  },
+  twitter: {
+    handle: '@__moniet',
+    cardType: 'summary_large_image'
+  }
+}
+
+export default nextSeoConfig
