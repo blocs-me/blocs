3c0c109515d06e3bcf555ebddb0310380cfd5b8f
Moniet <moniet.sawhney@gmail.com>
2023-02-10 10:45:58 +0100

    📝 new product + dollar options

--- src/constants/stripePriceIds.ts
+++ src/constants/stripePriceIds.ts
@@ -1,24 +1,60 @@
 import type { ProductTitles } from '@/gtypes/stripe'
 
 type PriceIds = {
-  [P in ProductTitles]?: string
-} & {
-  legacyLifetimeAccess?: string
+  [P in ProductTitles | 'legacyLifetimeAccess']?: {
+    [K in 'dollars' | 'euros']: string
+  }
 }
 
 const local: PriceIds = {
-  lifetimeAccess: 'price_1MQZs3HyXnRceQpOUY4FjNnR',
-  waterTracker: 'price_1MQbKkHyXnRceQpOX9mZZL7N',
-  habitTracker: 'price_1MQaw6HyXnRceQpOYLPoWdAS',
-  pomodoro: 'price_1MQawTHyXnRceQpOILT0ITBq'
+  lifetimeAccess: {
+    euros: 'price_1MZiiLHyXnRceQpOEgCe4oC8',
+    dollars: 'price_1MQdeMHyXnRceQpOE2YYC6VP'
+  },
+  waterTracker: {
+    euros: 'price_1MQbKkHyXnRceQpOX9mZZL7N',
+    dollars: 'price_1MZt1GHyXnRceQpOaRyI9iLi'
+  },
+  habitTracker: {
+    euros: 'price_1MQaw6HyXnRceQpOYLPoWdAS',
+    dollars: 'price_1MZijWHyXnRceQpOYxlsHUCi'
+  },
+  pomodoro: {
+    euros: 'price_1MQawTHyXnRceQpOILT0ITBq',
+    dollars: 'price_1MZryuHyXnRceQpO03FvIJ6X'
+  },
+  unlimitedAccess: {
+    euros: 'price_1MZiPaHyXnRceQpOIdQvOHIh',
+    dollars: 'price_1MY3x1HyXnRceQpOJBTIK4Ph'
+  }
 }
 
 const prod: PriceIds = {
-  lifetimeAccess: 'price_1MXMgYHyXnRceQpOw5BY6Xxw',
-  legacyLifetimeAccess: 'price_1MQWR6HyXnRceQpOpEBmf9Te', // needed to add this for the validation for preventing 2nd time purchase of the same product
-  waterTracker: 'price_1MQWXtHyXnRceQpOyPLvj3gW',
-  habitTracker: 'price_1MQWVtHyXnRceQpOcvXB73Mv',
-  pomodoro: 'price_1MQWTjHyXnRceQpOMxQBvlW3'
+  lifetimeAccess: {
+    euros: 'price_1MXMgYHyXnRceQpOw5BY6Xxw',
+    dollars: 'price_1MQWR6HyXnRceQpOpEBmf9Te'
+  },
+  legacyLifetimeAccess: {
+    // needed to add this for the validation for preventing 2nd time purchase of the same product
+    dollars: 'price_1MQWR6HyXnRceQpOpEBmf9Te',
+    euros: ''
+  },
+  waterTracker: {
+    euros: 'price_1MQWXtHyXnRceQpOyPLvj3gW',
+    dollars: 'price_1MZt3YHyXnRceQpOyT4nzgGl'
+  },
+  habitTracker: {
+    euros: 'price_1MQWVtHyXnRceQpOcvXB73Mv',
+    dollars: 'price_1MZs59HyXnRceQpOTyDWMY3I'
+  },
+  pomodoro: {
+    euros: 'price_1MQWTjHyXnRceQpOMxQBvlW3',
+    dollars: 'price_1MZs5wHyXnRceQpODjEQdjC3'
+  },
+  unlimitedAccess: {
+    euros: 'price_1MZig7HyXnRceQpOa53IeyCb',
+    dollars: 'price_1MY5p3HyXnRceQpOPn3jrwnl'
+  }
 }
 
 const stripePriceIds =
--- src/constants/stripeProductIds.ts
+++ src/constants/stripeProductIds.ts
@@ -7,14 +7,16 @@ const production = {
   lifetimeAccess: 'prod_NAsBHn5G2gY7YR',
   waterTracker: 'prod_NAsIXOtxrA0jDS',
   habitTracker: 'prod_NAsGMR57WW9rFj',
-  pomodoro: 'prod_NAsEG9NZW2CWGI'
+  pomodoro: 'prod_NAsEG9NZW2CWGI',
+  unlimitedAccess: 'prod_NIfHOoMMB6Nc86'
 } as ProductIds
 
 const local = {
   lifetimeAccess: 'prod_NAvjZe37uqV8lm',
   waterTracker: 'prod_NAxFsXZbbI5Nug',
   pomodoro: 'prod_NAwqtwT4Ze0n3S',
-  habitTracker: 'prod_NAwpIE4Kv2yyZR'
+  habitTracker: 'prod_NAwpIE4Kv2yyZR',
+  unlimitedAccess: 'prod_NIhDz1GSCAdEBH'
 } as ProductIds
 
 const stripeProductIds: Partial<ProductIds> =
