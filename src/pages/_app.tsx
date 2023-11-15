77eea3665d078de56d0a153a565c9e84f5f9efaa
Moniet <moniet.sawhney@gmail.com>
2023-11-15 15:24:02 +0100

    ✨ restore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,22 @@ function MyApp({ Component, pageProps }) {
             data-cf-beacon='{"token": "a9429c8dbd674c38bf769344e4abc7d8"}'
           />
         )}
+
+        {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && (
+          <script
+            type="text/javascript"
+            defer
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "foxjkrf7xa");
+            `
+            }}
+          />
+        )}
       </Head>
       <GlobalProvider>
         <Reset />
