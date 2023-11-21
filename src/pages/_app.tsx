5c1ec136625551e08381ea0125a227ca18b1546c
Santos-Apps <Daniel.santos.business@gmail.com>
2023-11-21 07:18:08 +0100

    replace another analytics tool with plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,28 +10,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        {process.env.NODE_ENV !== 'development' && (
-          <script
-            defer
-            src="https://static.cloudflareinsights.com/beacon.min.js"
-            data-cf-beacon='{"token": "a9429c8dbd674c38bf769344e4abc7d8"}'
-          />
-        )}
-
         {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && (
-          <script
-            type="text/javascript"
-            defer
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "foxjkrf7xa");
-            `
-            }}
-          />
+          <script defer data-domain="blocs.me" src="https://plausible.io/js/script.js"></script>
         )}
       </Head>
       <GlobalProvider>
