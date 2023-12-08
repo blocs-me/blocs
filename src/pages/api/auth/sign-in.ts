1bbb02bfc96beb4ea5bdad8db6c7f2a33f9c5603
Santos-Apps <Daniel.santos.business@gmail.com>
2023-12-08 21:36:26 +0100

    fix issues

--- src/pages/api/auth/sign-in.ts
+++ src/pages/api/auth/sign-in.ts
@@ -13,7 +13,8 @@ import Stripe from 'stripe'
 import { getCurrentISOString } from '@/utils/dateUtils/getCurrentISOString'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2022-11-15'
+  apiVersion: '2023-08-16',
+  typescript: true
 })
 
 const getClientUserData = (blocsUser: BlocsUserServer) => ({
--- src/pages/api/payments/customer-portal-session.ts
+++ src/pages/api/payments/customer-portal-session.ts
@@ -3,7 +3,8 @@ import getBlocsUser from '@/lambda/middlewares/getBlocsUser'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2022-11-15'
+  apiVersion: '2023-08-16',
+  typescript: true
 })
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
