03c89783182d18aa5c2072db3b8a492ce0b5d8d4
Santos-Apps <Daniel.santos.business@gmail.com>
2023-12-18 07:33:14 +0100

    introduced hotjar to collect feedback

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,32 @@ import GlobalStyle from '../styles/GlobalStyle'
 import Reset from '../styles/Reset'
 import theme from '../styles/theme'
 import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && (
+        {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' || true && (
+          <>
           <script defer data-domain="blocs.me" src="https://plausible.io/js/script.tagged-events.js"></script>
+          <Script
+              id="hotjar-widget"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3797364,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                  `,
+              }}
+            />
+          </>
         )}
       </Head>
       <GlobalProvider>
